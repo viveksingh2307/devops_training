7f1ff5310f8eee559bdabd5823a868e485863309
viveksingh2307 <114875512+viveksingh2307@users.noreply.github.com>
2025-03-21 10:33:24 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "vivek_resource_group" {
+    name = "vivek_rg-0"
+    location = "east us"
+}
---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "azurerm" {
+    features {}
+    
+subscription_id = "38bd44d5-d91c-4f34-8f1e-c946d6697ae4"
+}
