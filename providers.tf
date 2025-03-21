c276ca02bd7bde235b812990b1bde3af0afa8434
viveksingh2307 <114875512+viveksingh2307@users.noreply.github.com>
2025-03-21 12:34:42 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,12 +3,3 @@ provider "azurerm" {
     
 subscription_id = "38bd44d5-d91c-4f34-8f1e-c946d6697ae4"
 }
-
-terraform {
-   backend "azurerm" {
-     resource_group_name   = "my-resource-group"  # The resource group containing the storage account
-     storage_account_name  = "mytfstateaccount"   # The name of the storage account
-     container_name        = "terraform-state"    # The name of the container to store the state file
-     key                    = "terraform.tfstate" # The name of the state file
-   }
-}
